3ba05224fb0e1e2bc5cda4710dd7b1b747e596ea
shui <164635470@qq.com>
2020-03-16 23:44:11 +0800

    详情页

--- mpcode/pages/home/home/home.js
+++ mpcode/pages/home/home/home.js
@@ -12,12 +12,19 @@ Component({
 		scrollLeft:0
   },
 	methods:{
+		//跳到详情页
+		toDetail(e){
+			console.log('跳转')
+			wx.navigateTo({
+				url:'../home/detail/detail',
+			})
+		},
 		tabSelect: function(e) {
 			this.setData({
 				TabCur: e.currentTarget.dataset.id,
 				scrollLeft: (e.currentTarget.dataset.id-1)*60
 			})
-		},
+		}, 
 		isCard: function(e) {
 		    console.log(e.detail.value)
 		    this.setData({
